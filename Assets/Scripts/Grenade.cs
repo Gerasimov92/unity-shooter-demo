7d80eea9269280abaf6a6e84dc805a9c531db772
Ivan Gerasimov <aka.montoya@gmail.com>
2021-04-11 19:03:12 +0300

    Initial commit

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grenade : Projectile
+{
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.CompareTag(seekTag))
+			Hit(col);
+	}
+}
